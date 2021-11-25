dc9b37227ef440a549022a8d6c813de7c8274246
lan <lan@xmind.net>
2021-11-25 11:22:11 +0800

    test

--- Swiftui-memo/View/Note/NoteView.swift
+++ Swiftui-memo/View/Note/NoteView.swift
@@ -64,6 +64,7 @@ struct NoteView: View {
                 try viewContext.save()
             } catch {
                 let nsError = error as NSError
+            
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
         }
@@ -76,8 +77,7 @@ struct NoteView: View {
             do {
                 try viewContext.save()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+               
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
